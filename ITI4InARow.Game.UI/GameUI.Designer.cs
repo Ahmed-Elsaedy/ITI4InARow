ce107537ddd07ad027e87db0acf33fbf62036826
Red Hawk <amr.ashraf.student@pua.edu.eg>
2018-02-09 14:32:59 +0200

    Change Icon 2

--- ITI4InARow.Game.UI/GameUI.Designer.cs
+++ ITI4InARow.Game.UI/GameUI.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameUI));
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
             this.ovalShape42 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
             this.ovalShape41 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
@@ -764,9 +765,11 @@
             this.ClientSize = new System.Drawing.Size(788, 622);
             this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GameUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GameUI";
             this.Load += new System.EventHandler(this.GameUI_Load);
             this.ResumeLayout(false);
