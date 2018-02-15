96ce334c0bf163063733a8798f2b65dc0dd9534c
Red Hawk <amr.ashraf.student@pua.edu.eg>
2018-02-15 22:57:40 +0200

    trying to fix position bug

--- ITI4InARow.Game.Client/GameUI.cs
+++ ITI4InARow.Game.Client/GameUI.cs
@@ -157,42 +157,42 @@ namespace ITI4InARow.Game.UI
             TokenColor = player2Color;
             if (TokenPosition >= 1 && TokenPosition <= 6)
             {
-                if (columns[0] > 0)
+                if (columns[0]-1 > 0)
                 {
                     columns[0]--;
                 }
             }
             else if (TokenPosition >= 7 && TokenPosition <= 12)
             {
-                if (columns[1] > 6)
+                if (columns[1]-1 > 6)
                 {
                     columns[1]--;
                 }
             }
             else if (TokenPosition >= 13 && TokenPosition <= 18)
             {
-                if (columns[2] >12)
+                if (columns[2]-1 >12)
                 {
                     columns[2]--;
                 }
             }
             else if (TokenPosition >= 19 && TokenPosition <= 24)
             {
-                if (columns[3] > 18)
+                if (columns[3]-1 > 18)
                 {
                     columns[3]--;
                 }
             }
             else if (TokenPosition >= 25 && TokenPosition <= 30)
             {
-                if (columns[4] > 24)
+                if (columns[4]-1 > 24)
                 {
                     columns[4]--;
                 }
             }
             else if (TokenPosition >= 31 && TokenPosition <= 36)
             {
-                if (columns[5] > 30)
+                if (columns[5]-1 > 30)
                 {
                     columns[5]--;
                 }
@@ -200,13 +200,14 @@ namespace ITI4InARow.Game.UI
             }
             else if (TokenPosition >= 37 && TokenPosition <= 42)
             {
-                if (columns[6] > 36)
+                if (columns[6]-1 > 36)
                 {
                     columns[6]--;
                 }
             }
-            ((OvalShape)shapeContainer1.Shapes.get_Item(TokenPosition - 1)).FillColor = TokenColor;
-            ((OvalShape)shapeContainer1.Shapes.get_Item(TokenPosition - 1)).Enabled = false;
+            OvalShape ovelClicked = ((OvalShape)shapeContainer1.Shapes.get_Item(TokenPosition - 1));
+            ovelClicked.FillColor = TokenColor;
+            ovelClicked.Enabled = false;
         }
 
         bool GamePlan(OvalShape ovalClicked, ref int x, CheckPosition cp)
